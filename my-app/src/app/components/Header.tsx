c18fa66f70e160f0878ee1740b16c5b45425bf63
unknown <ivg33000@gmail.com>
2024-10-20 08:56:18 +0300

    files added

--- my-app/src/app/components/Header.tsx
+++ my-app/src/app/components/Header.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Header = () => {
   return (
     <header className="bg-white text-gray-800 shadow-md">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center py-4">
-          <div>
-            <h1 className="text-2xl font-bold">Ivan Georgiev</h1>
-            <p className="text-sm text-gray-600">Product Engineer | QA Automation Specialist</p>
+          <div className="flex items-center space-x-5">
+            <Image
+             src="/Profile.jpg"
+             alt="Ivan Georgiev"
+             width={60}
+             height={60}
+             style={{objectFit: "cover"}}
+             className="rounded-full"             
+            />
+            <div>
+              <h1 className="text-2xl font-bold">Ivan Georgiev</h1>
+              <p className="text-sm text-gray-600">Product Engineer | QA Automation Specialist</p>
+            </div>
           </div>
           <nav>
             <ul className="flex space-x-6">
